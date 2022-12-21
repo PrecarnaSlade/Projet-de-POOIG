33f25283b263c956dfc1a993382d433f52a15fba
Gemi08 <81690657+Gemi08@users.noreply.github.com>
2022-12-21 16:01:09 +0100

    Grid.isLegalMove

--- Common/Grid.java
+++ Common/Grid.java
@@ -4,10 +4,14 @@ import MathFuncAndObj.Position;
 
 public class Grid<E> extends InternalObject {
     private Tile[][] grid;
+    private int width;
+    private int height;
 
     public Grid(int pWidth, int pHeight) {
         super();
         grid = new Tile[pWidth][pHeight];
+        width= pWidth;
+        height= pHeight;
     }
 
     public Tile<E> getTileByPos(Position pPos) {
@@ -23,6 +27,53 @@ public class Grid<E> extends InternalObject {
     }
 
     public boolean isLegalMove(Tile t, Position p){
+        int x= p.getX();
+        int y=p.getY();
+
+        if(!this.isEmpty(x,y)) return false;
+        return matchSides(x,y);
+    }
+
+    private boolean isEmpty(int x,int y){
+        return grid[x][y]==null;
+    }
+
+    private boolean matchSides(int x, int y){
+        if(this.topExist(x,y) &&
+                !matchSide((E[]) grid[x][y].getSides().getUpSide(),(E[]) grid[x][y-1].getSides().getDownSide()))
+            return false;
+        if(this.rightExist(x,y) &&
+                !matchSide((E[]) grid[x][y].getSides().getRightSide(),(E[]) grid[x+1][y].getSides().getLeftSide()))
+            return false;
+        if(this.bottomExist(x,y) &&
+                !matchSide((E[]) grid[x][y].getSides().getDownSide(),(E[]) grid[x][y+1].getSides().getUpSide()))
+            return false;
+        if(this.leftExist(x,y) &&
+                !matchSide((E[]) grid[x][y].getSides().getLeftSide(),(E[]) grid[x-1][y].getSides().getRightSide()))
+            return false;
+        return true;
+    }
+
+    private boolean topExist(int x, int y){
+        return y-1>=0 && grid[x][y]!=null;
+    }
+
+    private boolean rightExist(int x, int y){
+        return x+1<width && grid[x][y]!=null;
+    }
+
+    private boolean bottomExist(int x, int y){
+        return y+1<height && grid[x][y]!=null;
+    }
+
+    private boolean leftExist(int x, int y){
+        return x-1>=0 && grid[x][y]!=null;
+    }
+
+    private boolean matchSide(E[] side1, E[] side2){
+        for(int i=0; i<side1.length;i++){
+            if(!side1[i].equals(side2[i])) return false;
+        }
         return true;
     }
 }
